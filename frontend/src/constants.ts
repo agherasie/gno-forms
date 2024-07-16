a7067848802e5f9e9a27883e71ba5d93635a9f93
agherasie <alexandru.gherasie@epitech.eu>
2024-07-16 21:45:59 +0200

    feat: env file

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,6 +1,23 @@
+import { from } from "env-var";
+
+const vars = {
+  VITE_CHAIN_ID: import.meta.env.VITE_CHAIN_ID,
+  VITE_CHAIN_RPC: import.meta.env.VITE_CHAIN_RPC,
+  VITE_REALM_PATH: import.meta.env.VITE_REALM_PATH,
+};
+
+const env = from(vars, {});
+
 export const constants = {
-  chainID: "test4",
-  chainRPC: "https://rpc.test4.gno.land",
-  realmPath:
-    "gno.land/r/g1w62226g8hykfmtuasvz80rdf0jl6phgxsphh5v/testing/forms",
+  chainID: env.get("VITE_CHAIN_ID").default("test4").asString(),
+  chainRPC: env
+    .get("VITE_CHAIN_RPC")
+    .default("https://rpc.test4.gno.land")
+    .asString(),
+  realmPath: env
+    .get("VITE_REALM_PATH")
+    .default(
+      "gno.land/r/g1w62226g8hykfmtuasvz80rdf0jl6phgxsphh5v/testing/forms"
+    )
+    .asString(),
 };
